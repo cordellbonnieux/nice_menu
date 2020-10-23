ace2f6e2c1d14f514b80f0bc4402025d608df61a
cordellbonnieux <cordellbonnieux@gmail.com>
2020-10-23 10:09:02 -0700

    Links are now created via an object array. And the logo area checks for an image and displays it. If there is no image, then it uses text. I also added lots of mock content.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,119 @@ import {populateLogo, populateMenu} from './populate'
 
 const content = document.getElementById('content')
 
+let logoText = {
+    title: "Placeholder Title",
+    link: '#',
+    }
+let logoImg = {
+    img:'', // attach a logo img if you want to use
+    link: '#',
+    }
 let menuItems = [
     {
         title: 'link 1',
-        url: '#'
+        url: '#',
+        id: 'menuLink1'
+    },
+    {
+        title: 'link 2',
+        url: '#',
+        id: 'menuLink2'
+    },
+    {
+        title: 'link 3',
+        url: '#',
+        id: 'menuLink3'
+    },
+    {
+        title: 'link 4',
+        url: '#',
+        id: 'menuLink4'
+    },
+    {
+        title: 'link 5',
+        url: '#',
+        id: 'menuLink5'
+    },
+    {
+        title: 'link 6',
+        url: '#',
+        id: 'menuLink6'
+    },
+    {
+        title: 'link 7',
+        url: '#',
+        id: 'menuLink7'
+    },
+    {
+        title: 'link 8',
+        url: '#',
+        id: 'menuLink8'
+    },
+    {
+        title: 'link 9',
+        url: '#',
+        id: 'menuLink9'
+    },
+    {
+        title: 'link 10',
+        url: '#',
+        id: 'menuLink10'
+    },
+    {
+        title: 'link 11',
+        url: '#',
+        id: 'menuLink11'
+    },
+    {
+        title: 'link 12',
+        url: '#',
+        id: 'menuLink12'
+    },
+    {
+        title: 'link 13',
+        url: '#',
+        id: 'menuLink13'
+    },
+    {
+        title: 'link 14',
+        url: '#',
+        id: 'menuLink14'
+    },
+    {
+        title: 'link 15',
+        url: '#',
+        id: 'menuLink15'
+    },
+    {
+        title: 'link 16',
+        url: '#',
+        id: 'menuLink16'
+    },
+    {
+        title: 'link 17',
+        url: '#',
+        id: 'menuLink17'
+    },
+    {
+        title: 'link 18',
+        url: '#',
+        id: 'menuLink18'
+    },
+    {
+        title: 'link 19',
+        url: '#',
+        id: 'menuLink19'
+    },
+    {
+        title: 'link 20',
+        url: '#',
+        id: 'menuLink20'
     },
 ]
 
 buildStructure(content)
-populateLogo(logoWrapper)
+populateLogo(logoWrapper, logoText, logoImg)
 populateMenu(menuWrapper, menuItems)
 
 import css from './style.css'
--- src/populate.js
+++ src/populate.js
@@ -1,10 +1,22 @@
-export function populateLogo(logo){
-
+export function populateLogo(wrapper, text, image){
+    if (image.img){
+        const imgLink = document.createElement('a')
+        wrapper.appendChild(image)
+        return
+    } else if (text){
+        const textWrapper = document.createElement('a')
+            textWrapper.setAttribute('id', 'websiteTitle')
+            textWrapper.textContent = text.title
+            textWrapper.href = text.link
+        wrapper.appendChild(textWrapper)
+        return
+    }
 }
 export function populateMenu(menu, items){
     for (let i = 0; i < items.length; i++){
         const btn = document.createElement('div')
             btn.setAttribute('class', 'menuBtn')
+            btn.setAttribute('id', items[i].id)
         const link = document.createElement('a')
             link.setAttribute('class', 'menuLink')
             link.textContent = items[i].title
